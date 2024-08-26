d1c4fb68330df0e69aa0bbfa7e497062e410e718
Loisarn <bolo21mv@student.ju.se>
2024-08-26 22:49:25 +0200

    small changes

--- Backend/server.js
+++ Backend/server.js
@@ -181,7 +181,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8081
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
