fabb267e7ae989cabbdf213017d252892fe9a80e
Loisarn <bolo21mv@student.ju.se>
2024-08-26 23:10:28 +0200

    changing maps in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -170,15 +170,15 @@ app.post('/posts', upload.array('images', 5), (req, res) => {
 
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 // Fallback route for React Router
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8081
